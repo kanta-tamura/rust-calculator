fe85bcaf70c340ed4bd15ce4d3fe5f65efba00de
PhiZero <kanta03085@outlook.jp>
2021-01-03 21:28:42 +0900

    add: "+", "-", "*", "/", "(", ")" tokens

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #[derive(Debug)]
 enum TokenKind {
     Num(i64),   // numerical value
+    Add,        // "+"
+    Sub,        // "-"
+    Mul,        // "*"
+    Div,        // "/"
+    LParen,     // "("
+    RParen,     // ")"
     EOF,        // terminal symbol
 }
 
@@ -21,18 +27,35 @@ impl Token {
 fn test_token() {
     let mut tokens = Vec::new();
     tokens.push(Token::new(TokenKind::Num(12)));
+    tokens.push(Token::new(TokenKind::Add));
+    tokens.push(Token::new(TokenKind::Sub));
+    tokens.push(Token::new(TokenKind::Num(4)));
+    tokens.push(Token::new(TokenKind::Mul));
+    tokens.push(Token::new(TokenKind::Div));
+    tokens.push(Token::new(TokenKind::Num(6)));
+    tokens.push(Token::new(TokenKind::LParen));
+    tokens.push(Token::new(TokenKind::RParen));
     tokens.push(Token::new(TokenKind::EOF));
 
-    let mut compere = Vec::new();
+    let mut compare_num = Vec::new();
+    let mut compere_symbol = Vec::new();
     for token in tokens {
         match token.kind {
-            TokenKind::Num(val) => { compere.push(val) }
-            TokenKind::EOF => {}
+            TokenKind::Num(val) => { compare_num.push(val) },
+            TokenKind::Add    => { compere_symbol.push("+") },
+            TokenKind::Sub    => { compere_symbol.push("-") },
+            TokenKind::Mul    => { compere_symbol.push("*") },
+            TokenKind::Div    => { compere_symbol.push("/") },
+            TokenKind::LParen => { compere_symbol.push("(") },
+            TokenKind::RParen => { compere_symbol.push(")") },
+            TokenKind::EOF    => { compere_symbol.push(" ") },
         }
     }
 
-    let result = vec![12];
-    assert_eq!(result, compere);
+    let result_num = vec![12, 4, 6];
+    let result_symbol = vec!["+", "-", "*", "/", "(", ")", " "];
+    assert_eq!(result_num, compare_num);
+    assert_eq!(result_symbol, compere_symbol);
 }
 
 fn main() {
